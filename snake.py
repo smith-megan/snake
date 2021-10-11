acb6ada6d974aec1216f8f175212908e50d60b9c
Megan <smith.megan.design@gmail.com>
2021-10-10 22:53:43 -0600

    running game no death at walls

--- snake.py
+++ snake.py
@@ -21,8 +21,13 @@ game_window=pygame.display.set_mode((window_x,window_y))
 fps= pygame.time.Clock()
 
 snake_position=[100,50]
-snake_body=[random.randrange(1, (window_x//10))*10,
-            random.randrange(1, (window_y//10))*10]
+snake_body=[[100,50],
+            [90,50],
+            [80,50],
+            [70,50]]
+
+fruit_position=[random.randrange(1, (window_x//10))*10,
+                random.randrange(1, (window_y//10))*10]
 
 fruit_spawn=True
 
@@ -70,6 +75,17 @@ while True:
       if event.key==pygame.K_RIGHT:
         change_to='RIGHT'
   
+
+  if change_to == 'UP' and direction != 'DOWN':
+    direction = 'UP'
+  if change_to == 'DOWN' and direction != 'UP':
+    direction = 'DOWN'
+  if change_to == 'LEFT' and direction != 'RIGHT':
+    direction = 'LEFT'
+  if change_to == 'RIGHT' and direction != 'LEFT':
+    direction = 'RIGHT'
+
+  
   if direction=='UP':
     snake_position[1]-=10
   if direction =='DOWN':
@@ -79,12 +95,35 @@ while True:
   if direction=='RIGHT':
     snake_position[0]+=10
   
+  snake_body.insert(0, list(snake_position))
+  if snake_position[0]== fruit_position[0] and snake_position[1] == fruit_position[1]:
+    score +=10
+    fruit_spawn=False
+  else:
+    snake_body.pop()
+
+  if not fruit_spawn:
+    fruit_position=[random.randrange(1, (window_x//10))*10,
+    random.randrange(1,(window_y//10))*10]
+
+  fruit_spawn=True
+  game_window.fill(black)
 
   for pos in snake_body:
-    pygame.draw.rect(game_window, green, 
-                    pygame.Rect(pos[0],pos[1],10,10))
-  # pygame.draw.rect(game_window, white, pygame.Rect())
+    pygame.draw.rect(game_window, green, pygame.Rect(pos[0],pos[1],10,10))
+  
+  
+  pygame.draw.rect(game_window, white, pygame.Rect(fruit_position[0], fruit_position[1],10,10))
 
+
+  if snake_position[0] < 0 or snake_position[0] > window_x-10:
+    snake_position=[100,50]
+  if snake_position[1]<0 or snake_position[1] > window_y-10:
+    snake_position=[100, 50]
+
+  for block in snake_body[1:]:
+    if snake_position[0] == block[0] and snake_position[1]==block[1]:
+      game_over()
 
   show_score(1, white, 'times new roman', 20)
 
